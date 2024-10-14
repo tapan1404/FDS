e6f32fba315f76ed1990d5d2e4e85be3529c3ede
tapan1404 <9810982396bhar@gmail.com>
2024-10-14 11:58:07 +0530

    day6

---
+++ day6/script.js
@@ -0,0 +1,17 @@
+// let a=() =>{
+//     console.log("Arrow Fun");
+// };
+// let b=()=>console.log("Arrow Fun2");
+// b();
+// a();
+// let c=(x,y)=>{
+//     return x+y;
+// }
+// let res=c("tapan","hello");
+// let resl=c(12,11);
+// console.log(res,resl)
+function display(){
+    let p1=document.querySelector('p');
+    p1.innerHTML="i am in para DOX";
+    p1.style.backgroundColor="green";
+}

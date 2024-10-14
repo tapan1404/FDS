6667174d84a6d0d2758e814f5a607d0cc4bff0b6
tapan1404 <9810982396bhar@gmail.com>
2024-10-14 12:21:36 +0530

    day6

--- day6/script.js
+++ day6/script.js
@@ -10,8 +10,22 @@
 // let res=c("tapan","hello");
 // let resl=c(12,11);
 // console.log(res,resl)
-function display(){
-    let p1=document.querySelector('p');
-    p1.innerHTML="i am in para DOX";
-    p1.style.backgroundColor="green";
+// function display(){
+//     let p1=document.querySelectorAll('p');
+//     p1[0].innerHTML="i am in para DOX";
+//     p1[0].style.backgroundColor="green";
+//     p1[1].innerHTML="i am in para DOX";
+//     p1[1].style.backgroundColor="red";
+// }
+// let changeh= () =>{
+//     let head1=document.querySelector('h1');
+//     head1.textContent="change the title";
+//     let a1=document.querySelector('a');
+//     a1.setAttribute("href","https://aktu.ac.in/");
+// }
+let addcontent=()=>{
+    const newd=document.createElement('div');
+    newd.textContent="this is new d";
+    const body=document.querySelector('body');
+    body.appendChild(newd);
 }
